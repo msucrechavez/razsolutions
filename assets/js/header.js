df4ab6b2424b8d2785c2eeda9ab309e78b5cea55
msucre <marcelo.sucre@gmail.com>
2026-02-11 23:50:47 -0500

    change header path

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,4 @@
-const basePath = window.location.hostname.includes("github.io")
-  ? "/razsolutions"
-  : "";
-
-fetch(`${basePath}/header.html`)
+fetch("razsolutions/header.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("header-placeholder").innerHTML = data;
